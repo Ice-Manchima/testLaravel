f685304815cc379d996f4b6d12afaac6cef41770
Ice-Manchima <manchima@peerpower.co.th>
2018-12-17 16:37:47 +0700

    Add Mutators of Loan
    Add mutators of Loan model for change String to date

--- app/Loan.php
+++ app/Loan.php
@@ -7,4 +7,27 @@ use Illuminate\Database\Eloquent\Model;
 class loan extends Model
 {
     //
+    protected $fillable = ['amount', 'term', 'interest_rate', 'start_date',];
+
+
+    /* //Accessors : User to retreive data from Database (with format etc.)
+    public function getAmountAttribute($value){
+        return number_format($value, 2);
+    }
+
+    public function getTermAttribute($value){
+        return number_format($value, 0);
+    }
+    */
+
+    //Mutators
+    public function setStartDateAttribute($value)
+    {
+        $this->attributes['start_date'] = date('Y-m-d', strtotime($value));
+    }
+
+    //Cast
+//    protected $casts = [
+//        'start_date' => 'datetime:Y-m',
+//    ];
 }
